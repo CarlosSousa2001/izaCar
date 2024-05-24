0181956d8f6f4222d7b98e2425e02921420484af
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-05-24 14:56:34 -0300

    first commit

---
+++ src/main/java/com/api/izacar/IzacarApplication.java
@@ -0,0 +1,13 @@
+package com.api.izacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzacarApplication.class, args);
+	}
+
+}
